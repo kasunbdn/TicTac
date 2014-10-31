7aa497e184d67ed123f00c235818d6af24f8f098
dinusha92 <dinudiss@gmail.com>
2014-10-31 19:43:22 +0530

    Second  algo for the game

--- TicTacToe/src/com/miniproject/tictactoe/tttgame.java
+++ TicTacToe/src/com/miniproject/tictactoe/tttgame.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 /*===========NO NEED=====================*/
 
-
 class player{
 	private char symbol;
 	private int location[];
@@ -13,6 +12,8 @@ class player{
 	public player(char sym){
 		symbol = sym;
 		location = new int[5];
+		i=0;
+		
 	}
 	public char getSymbol(){
 		return symbol;
@@ -42,7 +43,7 @@ class board{
 		}
 		
 	}
-	
+
 	/***Set place after giving player and the position**/
 	public void setMove(player p,int location){
 		if(positions[location]==empty){
@@ -56,7 +57,7 @@ class board{
 		
 	}
 
-	
+
 	/***Make the movement auto by the machine***/
 	public void AutoMove(player p,String level){
 		int j='\0';
@@ -66,10 +67,81 @@ class board{
 			}while(positions[j]!=empty);
 			setMove(p, j);
 		}
-		
+		if(level.equalsIgnoreCase("hard")){
+			if(checkPattern()!=10){
+				setMove(p,checkPattern());
+			}
+			else{
+				AutoMove(p, "easy");
+			}
+		}
 	
 	}
-
+	/*****Analyze the board to obtain next move****/
+	public int checkPattern(){
+		int loc=10;
+		for(int i=0;i<3;i++){
+		
+			if(positions[i]==positions[i+3] &&  positions[i+6]==empty && positions[i]!=empty ){
+				loc = i+6;
+				break;
+			}
+			else if(positions[i+6]==positions[i+3] &&  positions[i]==empty && positions[i+3]!=empty ){
+				loc = i;
+				break;
+			}
+			else if(positions[i]==positions[i+6] &&  positions[i+3]==empty && positions[i+6]!=empty){
+				loc = i+3;
+				break;
+				
+			}
+			else if(positions[3*i]==positions[3*i+1] && positions[3*i+2]==empty&& positions[3*i]!=empty){
+				loc = 3*i+2;
+				break;
+			}
+			else if(positions[3*i+1]==positions[3*i+2] &&positions[3*i]==empty&& positions[3*i+1]!=empty){
+				loc = 3*i;
+				break;
+			}
+			else if(positions[3*i]==positions[3*i+2] &&positions[3*i+1]==empty&& positions[3*i+2]!=empty){
+				loc = 3*i+1;
+				break;
+			}
+			
+			else if(positions[4]!=empty ){
+				if(positions[0]==positions[4] && positions[8]==empty){
+					loc = 8;
+					break;
+				}
+				if(positions[8]==positions[4] && positions[0]==empty){
+					loc = 0;
+				}
+				if(positions[2]==positions[4] && positions[6]==empty){
+					loc = 6;
+					break;
+				}
+				if(positions[6]==positions[4] && positions[2]==empty){
+					loc = 2;
+				}
+			}
+			else if(positions[0]!=empty ){
+				if(positions[0]==positions[8] && positions[4]==empty){
+					loc = 4;
+					break;
+				}
+				
+			}
+			else if(positions[2]!=empty ){
+				if(positions[2]==positions[6] && positions[4]==empty){
+					loc = 4;
+					break;
+				}
+				
+			}
+			/*------------------------------*/
+		}
+		return loc;
+	}
 	/****Check whether game is winning***/
 	public boolean checkWinning(){
 		boolean state = false;
